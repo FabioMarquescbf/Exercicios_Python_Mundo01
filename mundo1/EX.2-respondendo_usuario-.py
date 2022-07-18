447a6a0fee88b78bed5e2fd76e5bfdd489e4258d
Fábio Marques <108775609+FabioMarquescbf@users.noreply.github.com>
2022-07-18 16:37:53 -0300

    commit

---
+++ mundo1/EX.2-respondendo_usuario-.py
@@ -0,0 +1,4 @@
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+nome = str(input('Digite seu nome: '))
+print(f'é um prazer te conhecer {nome}, boas-vindas.')
---
+++ mundo1/EX.3-somando_dois_numeros-.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+valor1 = int(input('Digite um valor: '))
+valor2 = int(input('Digite outro valor: '))
+soma = valor1 + valor2
+print(f'A soma entre {valor1} e {valor2} é {soma}')
